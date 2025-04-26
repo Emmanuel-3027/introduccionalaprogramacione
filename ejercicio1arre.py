98c23760f55cd1ce3751199475570ab81c3e66d3
Emmanuel-3027 <monteromenaemmanuel@gmail.com>
2025-04-26 17:49:47 -0600

    Examen3_Emmanuel.py

---
+++ ejercicio1arre.py
@@ -0,0 +1,6 @@
+frutas_favoritas= ["sandia", "uva", "naranja", "mango", "piña"]
+print=(frutas_favoritas[0])
+print=(frutas_favoritas[1])
+print=(frutas_favoritas[2])
+print=(frutas_favoritas[3])
+print=(frutas_favoritas[4])
